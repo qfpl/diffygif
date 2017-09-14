c196319485fb39af5e74c152f23767752cf336a7
Dave Laing <dave.laing.80@gmail.com>
2017-09-14 17:31:52 +1000

    Adds some test data

---
+++ test-data-2/test01.hs
@@ -0,0 +1,24 @@
+todoItem ::
+  MonadWidget t m =>
+  TodoItemConfig ->
+  m (TodoItem t)
+todoItem (TodoItemConfig initialValue) =
+  elClass "div" "todo-item" $ mdo
+
+    cb <- checkbox False def
+    let
+      dComplete = cb ^. checkbox_value
+      mkCompleteClass False = ""
+      mkCompleteClass True  = "completed "
+
+    elDynClass "div" dRemoveClass $
+      text initialValue
+
+    eRemove <- button "Remove"
+
+    dRemoveClass <- holdDyn "" $
+      "removed " <$ eRemove
+
+    pure $
+      TodoItem dComplete eRemove
+
